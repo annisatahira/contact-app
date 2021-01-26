d7ab2f3a0b05876eebb3a572e22fff55667fd64f
annisa <annisa@gigaming.io>
2021-01-26 13:44:07 +0700

    add navbar

---
+++ contact-app/src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./style.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">
+        Kelola Kontak
+      </div>
+      <button>
+
+      </button>
+    </nav>
+  )
+};
+
+export default Navbar;
