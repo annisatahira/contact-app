b59fb2554f4ffe123318db1d7e225f6f6aa08fb8
annisa <annisa@gigaming.io>
2021-01-26 14:30:22 +0700

    add contact item

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Contact from './components/Card/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Contact />
+      <Contact />
     </>
   );
 }
---
+++ contact-app/src/components/Card/Contact/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./style.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <div className="contact-image">
+        <img src="/image.jpg" />
+      </div>
+      <div className="contact-name">Annisa Tahira</div>
+    </div>
+  )
+};
+
+export default Contact;
